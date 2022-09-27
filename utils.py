ea4328af4d914c98ed6e04f5b460de483ac94c7a
Renan Andrade <Fel9.renan02@gmail.com>
2022-09-27 18:27:26 -0300

    Moved functions to utils file

---
+++ utils.py
@@ -0,0 +1,56 @@
+import os
+import smtplib
+import urllib.request
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def envia_email(
+    texto,
+    titulo="Alerta de preço!",
+    origem="fel9.renan02@gmail.com",
+    destino="renan.fas02@gmail.com",
+):
+    """
+    Envia email usando o serviços do Gmail.
+    obrigatório: texto
+    opcional: titulo, origem, destino
+    """
+    usuario = os.getenv("GOOGLE_APP_EMAIL")
+    senha = os.getenv("GOOGLE_APP_PASSWORD")
+
+    mensagem = MIMEText(texto)
+    mensagem["Subject"] = titulo
+    mensagem["From"] = origem
+    mensagem["To"] = destino
+
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server.ehlo()
+    server.login(usuario, senha)
+    server.sendmail(origem, [destino], mensagem.as_string())
+    server.close()
+    print(f"Email enviado para {destino}")
+
+
+def retorna_texto_de_url(url):
+    """
+    Dada uma url, retorna o conteúdo em texto
+    """
+    pagina = urllib.request.urlopen(url)
+    return pagina.read().decode("utf-8")
+
+
+def encontra_trecho_com_preco(texto, tamanho_trecho=10):
+    """
+    Encontra o preço em dólares no texto e retorna o trecho contendo-o
+    """
+    posicao_simbolo_dolar = texto.index("$")
+    trecho = texto[posicao_simbolo_dolar + 1 : posicao_simbolo_dolar + tamanho_trecho]
+
+    separadores = [",", "."]
+    elementos_preco = [
+        char for char in trecho if char in separadores or char.isnumeric()
+    ]
+    return "".join(elementos_preco)
